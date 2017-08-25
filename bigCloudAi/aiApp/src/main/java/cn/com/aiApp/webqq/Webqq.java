3af7918877df758f1700d2ef317c534666a9905b
zhongjinkai766597324@qq.com <zhongjinkai766597324@qq.com>
2017-08-25 15:10:27 +0800

    验证二维码

--- bigCloudAi/aiApp/src/main/java/cn/com/aiApp/webqq/Webqq.java
+++ bigCloudAi/aiApp/src/main/java/cn/com/aiApp/webqq/Webqq.java
@@ -9,25 +9,81 @@ public class Webqq {
 	WebqqSocket webqqSocket=new WebqqSocket();
 	public static void main(String[] args) {
 		Webqq webqq = new Webqq();
-		webqq.getLoginImg();
-		try {
+		//webqq.getInitCookie();
+		/*try {
 			Thread.sleep(3000l);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}*/
+		webqq.getLoginImg();
+		System.out.println(webqq.webqqSocket.cookieQrsig);
+		try {
+			Thread.sleep(1000l);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		System.out.println(webqq.webqqSocket.cookieQrsig);
-		webqq.checkLogin();
+		boolean flag = true;
+		while(flag){
+			try {
+				Thread.sleep(1000l);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			webqq.checkLogin();
+		}
 		
 	}
 	
-	public void getLoginImg(){
+	public void getInitCookie(){
 		try {
-			Socket qqSocket = new Socket("121.51.141.121",80);
+			Socket qqSocket = new Socket("ui.ptlogin2.qq.com",80);
 			webqqSocket= new WebqqSocket(qqSocket);
 			new Thread(webqqSocket).start(); 
 			DataOutputStream dos = new DataOutputStream(qqSocket.getOutputStream()); 
+			dos.writeBytes("GET https://ui.ptlogin2.qq.com/cgi-bin/login?daid=164&target=self&style=16"
+					+ "&mibao_css=m_webqq&appid=501004106&enable_qlogin=0&no_verifyimg=1"
+					+ "&s_url=http%3A%2F%2Fw.qq.com%2Fproxy.html&f_url=loginerroralert"
+					+ "&strong_login=1&login_state=10&t=20131024001 HTTP/2.0\r\n");
+			dos.writeBytes("Host: ui.ptlogin2.qq.com\r\n");
+			dos.writeBytes("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) "
+					+ "Gecko/20100101 Firefox/55.0\r\n");
+			dos.writeBytes("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n");
+			dos.writeBytes("Accept-Language: zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3\r\n");
+			dos.writeBytes("Accept-Encoding: gzip, deflate, br\r\n");
+			dos.writeBytes("Referer: https://xui.ptlogin2.qq.com/cgi-bin/xlogin?daid=164&target=self&style=40"
+					+ "&mibao_css=m_webqq&appid=501004106&enable_qlogin=0&no_verifyimg=1&"
+					+ "s_url=http%3A%2F%2Fw.qq.com%2Fproxy.html&f_url=loginerroralert&strong_login=1&"
+					+ "login_state=10&t=20131024001\r\n");
+			dos.writeBytes("Cookie: pgv_pvid=9799748477; pac_uid=0_59785bcdd778a;"
+					+ " pt_guid_sig=544008898e91c21635d3b1be3be34c1f676618f7b663c2ea392723ee239ae0e0;"
+					+ " pgv_pvi=7303786496; pt2gguin=o0766597324;"
+					+ " pt_recent_uins=ad0fd504b9c461c368dcbdb1160a2b51c66b27e03ecf8525c947d4d6679d4e4d74e6402567f79829667d6e940522deede26b07139f066c76;"
+					+ " RK=jSGXfSA7Sb; ptcz=7b4105e3258369c8125ac7a8f36250e4783c0f55a1a3d46706430294cd499d89; "
+					+ "qrsig=hS-G3OkSgswoa3U-zWm3ApGmjBasKFP6kk8REFSSKoJ0gGNl*n7nLKnNLm72Xisr; pgv_si=s6388849664\r\n");
+			dos.writeBytes("Connection: keep-alive\r\n");
+			dos.writeBytes("Upgrade-Insecure-Requests: 1\r\n");
+			dos.writeBytes("\r\n");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void getLoginImg(){
+		try {
+			Socket qqSocket = new Socket("ssl.ptlogin2.qq.com",80);
+			if(webqqSocket!=null){
+				webqqSocket.setWebqqSocket(qqSocket);
+			}else{
+				webqqSocket= new WebqqSocket(qqSocket);
+			}
+			new Thread(webqqSocket).start(); 
+			DataOutputStream dos = new DataOutputStream(qqSocket.getOutputStream()); 
 			dos.writeBytes("GET https://ssl.ptlogin2.qq.com/ptqrshow?appid=501004106&e=2&l=M&s=3&d=72&v=4"
-					+ "&t=0.01915999622963327&daid=164 HTTP/1.1\r\n");
+					+ "&t=0.9142399367333609&daid=164 HTTP/2.0\r\n");
 			dos.writeBytes("Host: ssl.ptlogin2.qq.com\r\n");
 			dos.writeBytes("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) "
 					+ "Gecko/20100101 Firefox/55.0\r\n");
@@ -39,8 +95,9 @@ public class Webqq {
 					+ "s_url=http%3A%2F%2Fw.qq.com%2Fproxy.html&f_url=loginerroralert&strong_login=1&"
 					+ "login_state=10&t=20131024001\r\n");
 			dos.writeBytes("Cookie: pt_login_sig=; "
-					+ "pt_clientip=0542701c9adf7612; pt_serverip=24b80abf0e8d672b; pt_local_token=-1290381635; "
-					+ "uikey=af1c787f877e80e3115d777413b79b6968a58818e061a0e9b7fbe456ad1c886f; "
+					+ "pt_clientip="+this.webqqSocket.getCookiePtClientIp()+"; pt_serverip="+this.webqqSocket.getCookiePtServerIp()+";"
+					+ " pt_local_token=-1290381635; "
+					+ "uikey="+this.webqqSocket.getCookieUikey()+"; "
 					+ "pt_guid_sig=3bc11545777b74b280aa695524ca9294141b1c8ee8d7904cd093cca6644c84f9; "
 					+ "pgv_pvi=286536704; pgv_si=s6915505152\r\n");
 			dos.writeBytes("Connection: keep-alive\r\n");
@@ -52,22 +109,23 @@ public class Webqq {
 		}
 	}
 	
-	
 	public void checkLogin(){
 		try {
-			Socket qqSocket = new Socket("121.51.141.121",80);
-//					"ssl.ptlogin2.qq.com",443);
-			webqqSocket= new WebqqSocket(qqSocket);
+			Socket qqSocket = new Socket("ssl.ptlogin2.qq.com",80);
+			if(webqqSocket!=null){
+				webqqSocket.setWebqqSocket(qqSocket);
+			}else{
+				webqqSocket= new WebqqSocket(qqSocket);
+			}
 			new Thread(webqqSocket).start(); 
 			DataOutputStream dos = new DataOutputStream(qqSocket.getOutputStream()); 
-			dos.writeBytes("GET https://ssl.ptlogin2.qq.com/ptqrlogin?u1=http%3A%2F%2Fw.qq.com%2Fproxy.html"
-					+ "&ptqrtoken="+1545312357
-					+ "&ptredirect=0"
-					+ "&h=1&t=1&g=1&from_ui=1&ptlang=2052"
-					+ "&action=0-0-"+System.currentTimeMillis()+"&js_ver=10228&js_type=1"
-					+ "&login_sig="
-					+ "&pt_uistyle=40&aid=501004106&daid=164"
-					+ "&mibao_css=m_webqq& HTTP/1.1\r\n");
+			dos.writeBytes("GET https://ssl.ptlogin2.qq.com/ptqrlogin?"
+					+ "ptqrtoken="+this.hash33()+"&webqq_type=10&remember_uin=1"
+					+ "&login2qq=1&aid=501004106&u1=http%3A%2F%2Fw.qq.com%2Fproxy.html%3Flogin2qq%3D1%26webqq_type%3D10"
+					+ "&ptredirect=0&ptlang=2052&daid=164&from_ui=1&pttype=1&dumy=&fp=loginerroralert"
+					+ "&action=0-0-273540&mibao_css=m_webqq&t=undefined&g=1&js_type=0"
+					+ "&js_ver=10228&login_sig="
+					+ "&pt_randsalt=0 HTTP/2.0\r\n"); 
 			dos.writeBytes("Host: ssl.ptlogin2.qq.com\r\n");
 			dos.writeBytes("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) "
 					+ "Gecko/20100101 Firefox/55.0\r\n");
@@ -78,15 +136,18 @@ public class Webqq {
 					+ "&mibao_css=m_webqq&appid=501004106&enable_qlogin=0&no_verifyimg=1&"
 					+ "s_url=http%3A%2F%2Fw.qq.com%2Fproxy.html&f_url=loginerroralert&strong_login=1&"
 					+ "login_state=10&t=20131024001\r\n");
-			dos.writeBytes("Cookie: pt_login_sig=UWu3fBZnYDYi5fQ0N75acjpdnPy9JxFTFQ0XSWRWzOFk7goMDWAv1U4T5egKtcA6; "
-					+ "pt_clientip=0542701c9adf7612; pt_serverip=24b80abf0e8d672b; pt_local_token=-1290381635; "
-					+ "uikey=af1c787f877e80e3115d777413b79b6968a58818e061a0e9b7fbe456ad1c886f; "
-					+ "pt_guid_sig=3bc11545777b74b280aa695524ca9294141b1c8ee8d7904cd093cca6644c84f9; "
-					+ "pgv_pvi=286536704; pgv_si=s6915505152; qrsig="+this.webqqSocket.cookieQrsig+"\r\n");
+			dos.writeBytes("Cookie:"
+					/*+ " pgv_pvid=9799748477; pac_uid=0_59785bcdd778a;"
+					+ " pt_guid_sig=544008898e91c21635d3b1be3be34c1f676618f7b663c2ea392723ee239ae0e0;"
+					+ " pgv_pvi=7303786496; pt2gguin=o0766597324;"
+					+ " pt_recent_uins=ad0fd504b9c461c368dcbdb1160a2b51c66b27e03ecf8525c947d4d6679d4e4d74e6402567f79829667d6e940522deede26b07139f066c76;"
+					+ " RK=jSGXfSA7Sb;"
+					+ " ptcz=7b4105e3258369c8125ac7a8f36250e4783c0f55a1a3d46706430294cd499d89;"*/
+					+ " qrsig="+this.webqqSocket.cookieQrsig.trim()+";"
+					+ " pgv_si=s6388849664\r\n");
 			dos.writeBytes("Connection: keep-alive\r\n");
 			dos.writeBytes("Pragma: no-cache\r\n");
 			dos.writeBytes("Cache-Control: no-cache\r\n");
-			
 			dos.writeBytes("\r\n");
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
@@ -94,5 +155,16 @@ public class Webqq {
 			e.printStackTrace();
 		}
 	}
+	
+	public String hash33(){
+		String t = this.webqqSocket.cookieQrsig;
+		int e=0;
+		for(int i=0,n=t.length();n>i;++i){
+			e+=(e<<5)+t.charAt(i);
+		}
+		return (2147483647&e)+"";
+	}
+		
+	
 
 }
--- bigCloudAi/aiApp/src/main/java/cn/com/aiApp/webqq/WebqqSocket.java
+++ bigCloudAi/aiApp/src/main/java/cn/com/aiApp/webqq/WebqqSocket.java
@@ -4,25 +4,32 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.Socket;
 import java.net.SocketException;
 
 public class WebqqSocket implements Runnable {
-	
-	public String cookieQrsig="0E5f8OZ27ZM5Dro6RFRizU3fCfGJsjx9INjWWNBa8QsHVDmVuXyRm1v3y5GsO9cK";
-	
+
+	public String cookiePtUserId = "3312051082952809518";
+	public String cookiePtLoginSig = "PB4yRKweZ71KjaFNAvJM6bTHdCAEi9kAE2za*Nb-vOkmBL-2i5CDSuGGZzkEPNch";
+	public String cookiePtClientIp = "967edcf8f37ae9c8";
+	public String cookiePtServerIp = "a3980af492097aab";
+	public String cookieLoginParam = "daid%3D164%26target%3Dself%26style%3D16%26mibao_css%3Dm_webqq%26appid%3D501004106%26enable_qlogin%3D0%26no_verifyimg%3D1%26s_url%3Dhttp%253A%252F%252Fw.qq.com%252Fproxy.html%26f_url%3Dloginerroralert%26strong_login%3D1%26login_state%3D10%26t%3D20131024001";
+	public String cookieUikey = "dd40ef970b33ac795b73dd67912c430ae1a79f86714ff53ff175105bf9fc22bc";
+	public String cookiePtuiIdentifier = "000D54FAC028F09EB53AA632DDA353B5CD2FBA808C75A369FDC94CF6";
+
+	public String cookieQrsig = "xxxxxxxxxxxxxxxxxxxxxx";
+
 	public WebqqSocket(Socket webqqSocket) {
-		this.webqqSocket=webqqSocket;
+		this.webqqSocket = webqqSocket;
 	}
+
 	public WebqqSocket() {
 	}
-	
+
 	public Socket webqqSocket;
 	int contentLength;
-	int contentType;//0    1 png
+	int contentType;// 0 1 png
+
 	@Override
 	public void run() {
 		try {
@@ -30,31 +37,54 @@ public class WebqqSocket implements Runnable {
 		} catch (SocketException e) {
 		}
 		try {
-			DataInputStream dis = new DataInputStream(
-					this.webqqSocket.getInputStream());
+			DataInputStream dis = new DataInputStream(this.webqqSocket.getInputStream());
 			System.out.println("read");
-			String line = dis.readLine();
-			int i =0;
-	        while (line != null) {
-	        	System.out.println("line->"+line);
-	            line = dis.readLine();
-	            if ("".equals(line.trim())) {
-	            		break;
-	             }else if (line.indexOf("Content-Length") != -1) {
-	                 this.contentLength = Integer.parseInt(line.substring(line.indexOf("Content-Length") + 16));
-	             }else if(line.indexOf("image/png")!= -1){
-	            	 contentType=1;
-	             }else if(line.indexOf("Set-Cookie")!= -1){
-	            	String[] cookies= line.split(":")[1].split(";");
-	            	for (int j = 0; j < cookies.length; j++) {
-						if(cookies[j].indexOf("qrsig")!=-1){
-							this.cookieQrsig=cookies[j].split("=")[1];
+			String line = dis.readLine();// dis.readLine();
+			int i = 0;
+			while (line != null) {
+				System.out.println("line->" + line);
+				line = dis.readLine();
+				if ("".equals(line.trim())) {
+					break;
+				} else if (line.indexOf("Content-Length") != -1) {
+					this.contentLength = Integer.parseInt(line.substring(line.indexOf("Content-Length") + 16));
+				} else if (line.indexOf("image/png") != -1) {
+					contentType = 1;
+				}else if(line.indexOf("Content-Type: application/x-javascript;") != -1){
+					contentType = 0;
+				} else if (line.indexOf("Set-Cookie") != -1) {
+					String[] cookies = line.split(":")[1].split(";");
+					for (int j = 0; j < cookies.length; j++) {
+						if (cookies[j].indexOf("qrsig") != -1) {
+							this.setCookieQrsig(cookies[j].split("=")[1]);
 						}
+						if (cookies[j].indexOf("pt_user_id") != -1) {
+							this.setCookiePtUserId(cookies[j].split("=")[1]);
+						}
+						if (cookies[j].indexOf("pt_login_sig") != -1) {
+							this.setCookiePtLoginSig(cookies[j].split("=")[1]);
+						}
+						if (cookies[j].indexOf("pt_clientip") != -1) {
+							this.setCookiePtClientIp(cookies[j].split("=")[1]);
+						}
+						if (cookies[j].indexOf("pt_serverip") != -1) {
+							this.setCookiePtServerIp(cookies[j].split("=")[1]);
+						}
+						if (cookies[j].indexOf("login_param") != -1) {
+							this.setCookieLoginParam(cookies[j].split("=")[1]);
+						}
+						if (cookies[j].indexOf("uikey") != -1) {
+							this.setCookieUikey(cookies[j].split("=")[1]);
+						}
+						if (cookies[j].indexOf("ptui_identifier") != -1) {
+							this.setCookiePtuiIdentifier(cookies[j].split("=")[1]);
+						}
+						
 					}
-	             }
-	        }
-	        System.out.println("begin reading data......");
-	        readData(dis);
+				}
+			}
+			System.out.println("begin reading data......");
+			readData(dis);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -64,30 +94,119 @@ public class WebqqSocket implements Runnable {
 			}
 		}
 	}
-	
+
 	public void readData(DataInputStream reader) throws Exception {
-      switch (contentType) {
-	  case 0:
-		  String line="";
-		  while (line != null) {
-		  line = reader.readLine();
-		  System.out.println("line->"+line);
-		  }
-		break;
-	  case 1:
-		  File file = new File("C:/Users/Administrator/Desktop/cPic.png");
+		switch (contentType) {
+		case 0:
+			byte[] conbytes = new byte[this.contentLength];
+			reader.read(conbytes, 0, this.contentLength);
+			String con = new String(conbytes, "UTF-8");
+			System.out.println(con);
+			break;
+		case 1:
+			String cPic = "C:/Users/Administrator.2013-20160705FH/Desktop/cPic.png";
+			File file = new File(cPic);
 			FileOutputStream fos = new FileOutputStream(file);
 			int len;
 			byte[] b = new byte[this.contentLength];
-			len=reader.read(b,0,this.contentLength);
-			fos.write(b,0,len);
+			len = reader.read(b, 0, this.contentLength);
+			fos.write(b, 0, len);
 			fos.flush();
 			fos.close();
-			Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler file:///C:/Users/Administrator/Desktop/cPic.png");
-		break;
-	  default:
-		break;
+			Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler file:///" + cPic);
+			this.contentType = 0;
+			break;
+		default:
+			break;
+		}
 	}
+
+	public String getCookieQrsig() {
+		return cookieQrsig;
+	}
+
+	public void setCookieQrsig(String cookieQrsig) {
+		this.cookieQrsig = cookieQrsig;
+	}
+
+	public String getCookiePtUserId() {
+		return cookiePtUserId;
+	}
+
+	public void setCookiePtUserId(String cookiePtUserId) {
+		this.cookiePtUserId = cookiePtUserId;
+	}
+
+	public String getCookiePtLoginSig() {
+		return cookiePtLoginSig;
+	}
+
+	public void setCookiePtLoginSig(String cookiePtLoginSig) {
+		this.cookiePtLoginSig = cookiePtLoginSig;
+	}
+
+	public String getCookiePtClientIp() {
+		return cookiePtClientIp;
+	}
+
+	public void setCookiePtClientIp(String cookiePtClientIp) {
+		this.cookiePtClientIp = cookiePtClientIp;
+	}
+
+	public String getCookiePtServerIp() {
+		return cookiePtServerIp;
+	}
+
+	public void setCookiePtServerIp(String cookiePtServerIp) {
+		this.cookiePtServerIp = cookiePtServerIp;
+	}
+
+	public String getCookieLoginParam() {
+		return cookieLoginParam;
+	}
+
+	public void setCookieLoginParam(String cookieLoginParam) {
+		this.cookieLoginParam = cookieLoginParam;
+	}
+
+	public String getCookieUikey() {
+		return cookieUikey;
+	}
+
+	public void setCookieUikey(String cookieUikey) {
+		this.cookieUikey = cookieUikey;
+	}
+
+	public String getCookiePtuiIdentifier() {
+		return cookiePtuiIdentifier;
+	}
+
+	public void setCookiePtuiIdentifier(String cookiePtuiIdentifier) {
+		this.cookiePtuiIdentifier = cookiePtuiIdentifier;
+	}
+
+	public Socket getWebqqSocket() {
+		return webqqSocket;
+	}
+
+	public void setWebqqSocket(Socket webqqSocket) {
+		this.webqqSocket = webqqSocket;
+	}
+
+	public int getContentLength() {
+		return contentLength;
+	}
+
+	public void setContentLength(int contentLength) {
+		this.contentLength = contentLength;
+	}
+
+	public int getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(int contentType) {
+		this.contentType = contentType;
 	}
 
 }
