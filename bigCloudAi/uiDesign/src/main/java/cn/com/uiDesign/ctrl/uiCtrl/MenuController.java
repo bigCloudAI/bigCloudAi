400392cd58d7747cbc3e872266089a338f381c48
Administrator <Administrator@WIN-JHMD79LFEF6>
2017-09-04 23:41:03 +0800

    uidesign

---
+++ bigCloudAi/uiDesign/src/main/java/cn/com/uiDesign/ctrl/uiCtrl/MenuController.java
@@ -0,0 +1,26 @@
+package cn.com.uiDesign.ctrl.uiCtrl;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 项目各个界面的请求地址
+ *
+ * @since 2017-03-28
+ */
+@RestController
+@RequestMapping("/uiDesign")
+public class MenuController {
+
+	/*@RequestMapping(value ="/index",method = RequestMethod.GET)
+	public String index() {
+		return "index";
+	}*/
+	@RequestMapping(value ="/index",method = RequestMethod.GET)
+	 public ModelAndView login(){
+	        ModelAndView mv = new ModelAndView("index");
+	        return mv;
+	    }
+}
---
+++ bigCloudAi/uiDesign/src/main/resources/templates/uiDesign/README.md
@@ -0,0 +1,6 @@
+uiDesign
+========
+
+基于 bootstrap,vue 实现可视化组件布局编辑和配置工具 
+
+
