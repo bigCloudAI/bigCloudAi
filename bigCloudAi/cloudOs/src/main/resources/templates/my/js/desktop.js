f3a7b9c038da31f65d9d550976fed59c2467a417
Administrator <Administrator@WIN-JHMD79LFEF6>
2017-10-22 23:21:19 +0800

    win draw

--- bigCloudAi/cloudOs/src/main/resources/templates/my/js/desktop.js
+++ bigCloudAi/cloudOs/src/main/resources/templates/my/js/desktop.js
@@ -5,7 +5,8 @@ var app = new Vue({
 	ltrhide:false,
 	rtlshow:false,
 	rtlhide:false,
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    wins:[]
   },
   methods: {
 	    navswitch: function (event) {
@@ -22,6 +23,39 @@ var app = new Vue({
 	    	}
 	    	this.rtlshow=!this.rtlshow;
 	    	this.rtlhide=!this.rtlshow;
+	    },
+	    openWin:function(){
+	    	this.wins.push({name:this.wins.length});
+	    },
+	    closeWin:function(index){
+	    	this.wins.splice(index, 1);
 	    }
   }
 })
+
+
+//定义移动对象和移动坐标
+var Mouse_Obj="none",_x,_y;
+//拖动对象函数(自动)
+document.onmousemove=function()
+{
+ if(Mouse_Obj!=="none")
+ {
+(Mouse_Obj).style.left=_x+event.x;
+(Mouse_Obj).style.top=_y+event.y;
+ event.returnValue=false;
+ }
+}
+//停止拖动函数(自动)
+document.onmouseup=function()
+{
+ Mouse_Obj="none";
+}
+//确定被拖动对象函数 o为被拖动对象
+function m(o)
+{
+ Mouse_Obj=o;
+ _x=parseInt((Mouse_Obj).style.left)-event.x;
+ _y=parseInt((Mouse_Obj).style.top)-event.y;
+}
+
